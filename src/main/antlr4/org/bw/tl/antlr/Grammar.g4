e2d4697b90e302dedede563644df40d57281a2bb
BradleyWood <bradley.wood@uoit.net>
2018-05-07 19:59:04 -0400

    initial commit

---
+++ src/main/antlr4/org/bw/tl/antlr/Grammar.g4
@@ -0,0 +1,5 @@
+grammar Grammar;
+
+file
+    :
+    ;
---
+++ src/main/java/org/bw/tl/Application.java
@@ -0,0 +1,10 @@
+package org.bw.tl;
+
+import org.bw.tl.antlr.GrammarParser;
+
+public class Application {
+
+    public static void main(String[] args) {
+        GrammarParser g;
+    }
+}
