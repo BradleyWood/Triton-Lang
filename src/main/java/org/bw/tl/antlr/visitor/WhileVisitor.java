325f7cc9dca980fc8c23670c58caf775434a2981
BradleyWood <bradley.wood@uoit.net>
2018-05-12 18:19:25 -0400

    process while loop

---
+++ src/main/java/org/bw/tl/antlr/visitor/WhileVisitor.java
@@ -0,0 +1,30 @@
+package org.bw.tl.antlr.visitor;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.bw.tl.antlr.GrammarBaseVisitor;
+import org.bw.tl.antlr.GrammarParser;
+import org.bw.tl.antlr.ast.Expression;
+import org.bw.tl.antlr.ast.Node;
+import org.bw.tl.antlr.ast.WhileLoop;
+
+@RequiredArgsConstructor(staticName = "of")
+public class WhileVisitor extends GrammarBaseVisitor<WhileLoop> {
+
+    private final @Getter String sourceFile;
+
+    @Override
+    public WhileLoop visitWhileStatement(final GrammarParser.WhileStatementContext ctx) {
+        final Expression condition = ctx.condition.accept(ExpressionVisitor.of(sourceFile));
+        final Node body = ctx.body.accept(StatementVisitor.of(sourceFile));
+        final boolean doWhile = ctx.DO() != null;
+
+        final WhileLoop whileLoop = new WhileLoop(condition, body, doWhile);
+
+        whileLoop.setText(ctx.getText());
+        whileLoop.setLineNumber(ctx.start.getLine());
+        whileLoop.setFile(sourceFile);
+
+        return whileLoop;
+    }
+}
