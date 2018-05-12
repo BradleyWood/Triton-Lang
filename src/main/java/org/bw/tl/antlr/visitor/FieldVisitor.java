6ee2718f1626f172293441516aa269020df204ad
BradleyWood <bradley.wood@uoit.net>
2018-05-12 17:44:58 -0400

    process fields

---
+++ src/main/java/org/bw/tl/antlr/visitor/FieldVisitor.java
@@ -0,0 +1,28 @@
+package org.bw.tl.antlr.visitor;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import org.bw.tl.antlr.GrammarBaseVisitor;
+import org.bw.tl.antlr.GrammarParser;
+import org.bw.tl.antlr.ast.Expression;
+import org.bw.tl.antlr.ast.Field;
+
+@AllArgsConstructor(staticName = "of")
+public class FieldVisitor extends GrammarBaseVisitor<Field> {
+
+    private final @Getter String sourceFile;
+
+    @Override
+    public Field visitVarDef(final GrammarParser.VarDefContext ctx) {
+        final String name = ctx.IDENTIFIER().getText();
+        final Expression initialValue = ctx.expression().accept(ExpressionVisitor.of(sourceFile));
+
+        final Field field = new Field(name, initialValue);
+
+        field.setText(ctx.getText());
+        field.setLineNumber(ctx.start.getLine());
+        field.setFile(sourceFile);
+
+        return field;
+    }
+}
