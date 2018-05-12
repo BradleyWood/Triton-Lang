ad85fd4c71cf12534c8c64b221ed66040579f754
BradleyWood <bradley.wood@uoit.net>
2018-05-12 01:13:36 -0400

    remove unneeded constructor

--- src/main/java/org/bw/tl/antlr/ast/Module.java
+++ src/main/java/org/bw/tl/antlr/ast/Module.java
@@ -1,20 +1,17 @@
 package org.bw.tl.antlr.ast;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 public class Module {
 
     private final @Getter QualifiedName modulePackage;
     private final @Getter List<Field> fields;
-
-    public Module(final QualifiedName modulePackage, final List<Field> fields) {
-        this.modulePackage = modulePackage;
-        this.fields = fields;
-    }
 
     public Module(final QualifiedName modulePackage, final Field[] fields) {
         this(modulePackage, new ArrayList<>(Arrays.asList(fields)));
