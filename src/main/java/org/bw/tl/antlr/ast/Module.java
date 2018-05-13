51e7a571355ff592b0202f787435557bfe09ae0a
BradleyWood <bradley.wood@uoit.net>
2018-05-13 03:24:05 -0400

    update module representation

--- src/main/java/org/bw/tl/antlr/ast/Module.java
+++ src/main/java/org/bw/tl/antlr/ast/Module.java
@@ -1,19 +1,34 @@
 package org.bw.tl.antlr.ast;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
-@RequiredArgsConstructor
 public @Data class Module {
 
     private final QualifiedName modulePackage;
-    private final List<Field> fields;
+    private final List<File> files;
 
-    public Module(final QualifiedName modulePackage, final Field[] fields) {
-        this(modulePackage, new ArrayList<>(Arrays.asList(fields)));
+    /**
+     * Creates the representation of a module
+     *
+     * @param files The files that make up the module
+     * @return The module, if valid
+     */
+    public static Module of(final List<File> files) {
+        if (files == null || files.isEmpty())
+            return null;
+
+        final Iterator<File> it = files.iterator();
+        final QualifiedName packageName = it.next().getPackageName();
+
+        while (it.hasNext()) {
+            if (!Objects.equals(packageName, it.next().getPackageName()))
+                return null;
+        }
+
+        return new Module(packageName, files);
     }
 }
