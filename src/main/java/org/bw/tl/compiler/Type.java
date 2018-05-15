c91fa02702b2e0ab2b5a8dd1d7c6095b273f6081
BradleyWood <bradley.wood@uoit.net>
2018-05-14 21:55:57 -0400

    handle primitive and object types

---
+++ src/main/java/org/bw/tl/compiler/Type.java
@@ -0,0 +1,35 @@
+package org.bw.tl.compiler;
+
+import lombok.Data;
+import org.objectweb.asm.MethodVisitor;
+
+public abstract @Data class Type {
+
+    private final String desc;
+
+    public abstract boolean toInt(final MethodVisitor mv);
+
+    public abstract boolean toShort(final MethodVisitor mv);
+
+    public abstract boolean toChar(final MethodVisitor mv);
+
+    public abstract boolean toByte(final MethodVisitor mv);
+
+    public abstract boolean toLong(final MethodVisitor mv);
+
+    public abstract boolean toFloat(final MethodVisitor mv);
+
+    public abstract boolean toDouble(final MethodVisitor mv);
+
+    public abstract void toObject(final MethodVisitor mv);
+
+    public abstract void ret(final MethodVisitor mv);
+
+    public abstract void load(final MethodVisitor mv);
+
+    public abstract void store(final MethodVisitor mv);
+
+    public abstract void arrayLoad(final MethodVisitor mv);
+
+    public abstract void arrayStore(final MethodVisitor mv);
+}
