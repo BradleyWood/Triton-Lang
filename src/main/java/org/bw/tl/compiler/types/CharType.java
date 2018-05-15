b4c14a37d6b46fcbbb6bd931cd31fe824c32ae51
BradleyWood <bradley.wood@uoit.net>
2018-05-14 22:11:43 -0400

    char type

---
+++ src/main/java/org/bw/tl/compiler/types/CharType.java
@@ -0,0 +1,27 @@
+package org.bw.tl.compiler.types;
+
+import org.objectweb.asm.MethodVisitor;
+
+import static org.objectweb.asm.Opcodes.*;
+
+public class CharType extends IntType {
+
+    public CharType() {
+        super("C");
+    }
+
+    @Override
+    public boolean toChar(final MethodVisitor mv) {
+        return true;
+    }
+
+    @Override
+    public void arrayLoad(final MethodVisitor mv) {
+        mv.visitInsn(CALOAD);
+    }
+
+    @Override
+    public void arrayStore(final MethodVisitor mv) {
+        mv.visitInsn(CASTORE);
+    }
+}
