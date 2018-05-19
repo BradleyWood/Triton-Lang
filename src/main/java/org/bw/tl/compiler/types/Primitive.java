82a5ce84c3ad6ea75eadfa0bf426aa7b0de8e381
BradleyWood <bradley.wood@uoit.net>
2018-05-18 21:32:26 -0400

    map primitive type descriptors to helpers

--- src/main/java/org/bw/tl/compiler/types/Primitive.java
+++ src/main/java/org/bw/tl/compiler/types/Primitive.java
@@ -6,24 +6,26 @@ import org.jetbrains.annotations.Nullable;
 
 public enum Primitive {
 
-    INT("int", "I", "Ljava/lang/Integer;"),
-    SHORT("short", "S", "Ljava/lang/Short;"),
-    CHAR("char", "C", "Ljava/lang/Character;"),
-    BYTE("byte", "B", "Ljava/lang/Byte;"),
-    LONG("long", "J", "Ljava/lang/Long;"),
-    FLOAT("float", "F", "Ljava/lang/Float;"),
-    DOUBLE("double", "D", "Ljava/lang/Double;"),
-    VOID("void", "V", null),
-    BOOL("boolean", "Z", "Ljava/lang/Boolean;");
+    INT("int", "I", "Ljava/lang/Integer;", IntType.INSTANCE),
+    SHORT("short", "S", "Ljava/lang/Short;", ShortType.INSTANCE),
+    CHAR("char", "C", "Ljava/lang/Character;", CharType.INSTANCE),
+    BYTE("byte", "B", "Ljava/lang/Byte;", ByteType.INSTANCE),
+    LONG("long", "J", "Ljava/lang/Long;", LongType.INSTANCE),
+    FLOAT("float", "F", "Ljava/lang/Float;", FloatType.INSTANCE),
+    DOUBLE("double", "D", "Ljava/lang/Double;", DoubleType.INSTANCE),
+    VOID("void", "V", null, VoidType.INSTANCE),
+    BOOL("boolean", "Z", "Ljava/lang/Boolean;", BoolType.INSTANCE);
 
     private final @Getter String name;
     private final @Getter String desc;
     private final @Getter String wrappedType;
+    private final @Getter Type primitiveHelper;
 
-    Primitive(final String name, final String desc, final String wrappedType) {
+    Primitive(final String name, final String desc, final String wrappedType, final Type primitiveHelper) {
         this.name = name;
         this.desc = desc;
         this.wrappedType = wrappedType;
+        this.primitiveHelper = primitiveHelper;
     }
 
     @Nullable
