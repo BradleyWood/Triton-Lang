5bc64e7e3c787fb75ff59c7cf5f458718e562efb
BradleyWood <bradley.wood@uoit.net>
2018-06-06 11:18:42 -0400

    type descriptor for literal value

--- src/main/java/org/bw/tl/antlr/ast/Literal.java
+++ src/main/java/org/bw/tl/antlr/ast/Literal.java
@@ -2,6 +2,7 @@ package org.bw.tl.antlr.ast;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.bw.tl.compiler.types.*;
 
 @EqualsAndHashCode(callSuper = true)
 public @Data class Literal<T> extends Expression {
@@ -13,4 +14,25 @@ public @Data class Literal<T> extends Expression {
         visitor.visitLiteral(this);
     }
 
+    @Override
+    public String getTypeDesc() {
+        if (value instanceof String) {
+            return "Ljava/lang/String;";
+        } else if (value instanceof Double) {
+            return DoubleType.INSTANCE.getDesc();
+        } else if (value instanceof Float) {
+            return FloatType.INSTANCE.getDesc();
+        } else if (value instanceof Integer) {
+            int v = (int) value;
+            if (v <= Byte.MAX_VALUE && v > Byte.MIN_VALUE) {
+                return ByteType.INSTANCE.getDesc();
+            } else if (v <= Short.MAX_VALUE && v >= Short.MIN_VALUE) {
+                return ShortType.INSTANCE.getDesc();
+            }
+            return IntType.INSTANCE.getDesc();
+        } else if (value instanceof Boolean) {
+            return BoolType.INSTANCE.getDesc();
+        }
+        return null;
+    }
 }
