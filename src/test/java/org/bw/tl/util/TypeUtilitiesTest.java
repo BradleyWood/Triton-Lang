1dce98bbde48ad62227c214dc176b8870366778c
BradleyWood <bradley.wood@uoit.net>
2018-07-09 17:02:29 -0400

    impl basic type utility tests

---
+++ src/test/java/org/bw/tl/util/TypeUtilitiesTest.java
@@ -0,0 +1,72 @@
+package org.bw.tl.util;
+
+import org.bw.tl.antlr.ast.QualifiedName;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.bw.tl.util.TypeUtilities.getTypeFromName;
+
+public class TypeUtilitiesTest {
+
+    @Test
+    public void testGetTypeFromName() {
+        Assert.assertNotNull(getTypeFromName(new QualifiedName("java", "lang", "String")));
+        Assert.assertNotNull(getTypeFromName(new QualifiedName("java", "lang", "Integer")));
+
+        Assert.assertNotNull(getTypeFromName(new QualifiedName("int")));
+
+        Assert.assertNotNull(getTypeFromName("byte"));
+        Assert.assertNotNull(getTypeFromName("java.lang.Object"));
+
+        Assert.assertEquals(getTypeFromName(new QualifiedName("java", "lang", "Integer")),
+                getTypeFromName("java.lang.Integer"));
+    }
+
+    @Test
+    public void testIsAssignable() {
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("long"), getTypeFromName("long")));
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("int"), getTypeFromName("long")));
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("short"), getTypeFromName("long")));
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("byte"), getTypeFromName("long")));
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("char"), getTypeFromName("long")));
+
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("long"), getTypeFromName("int")));
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("int"), getTypeFromName("int")));
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("short"), getTypeFromName("int")));
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("char"), getTypeFromName("int")));
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("byte"), getTypeFromName("int")));
+
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("long"), getTypeFromName("short")));
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("int"), getTypeFromName("short")));
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("short"), getTypeFromName("short")));
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("char"), getTypeFromName("short")));
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("byte"), getTypeFromName("short")));
+
+
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("int"), getTypeFromName("boolean")));
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("boolean"), getTypeFromName("boolean")));
+
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("float"), getTypeFromName("double")));
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("double"), getTypeFromName("float")));
+    }
+
+    @Test
+    public void testIsAssignableNonPrimitives() {
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("java.lang.String"), getTypeFromName("java.lang.Object")));
+        Assert.assertFalse(TypeUtilities.isAssignableFrom(getTypeFromName("java.lang.Object"), getTypeFromName("java.lang.String")));
+        Assert.assertTrue(TypeUtilities.isAssignableFrom(getTypeFromName("java.lang.Integer"), getTypeFromName("java.lang.Number")));
+    }
+
+    @Test
+    public void testIsAssignableWithImplicitCast() {
+        Assert.assertTrue(TypeUtilities.isAssignableWithImplicitCast(getTypeFromName("float"), getTypeFromName("double")));
+        Assert.assertFalse(TypeUtilities.isAssignableWithImplicitCast(getTypeFromName("double"), getTypeFromName("float")));
+
+        Assert.assertTrue(TypeUtilities.isAssignableWithImplicitCast(getTypeFromName("int"), getTypeFromName("long")));
+        Assert.assertFalse(TypeUtilities.isAssignableWithImplicitCast(getTypeFromName("long"), getTypeFromName("int")));
+
+        Assert.assertFalse(TypeUtilities.isAssignableWithImplicitCast(getTypeFromName("double"), getTypeFromName("double")));
+        Assert.assertFalse(TypeUtilities.isAssignableWithImplicitCast(getTypeFromName("int"), getTypeFromName("int")));
+        Assert.assertFalse(TypeUtilities.isAssignableWithImplicitCast(getTypeFromName("byte"), getTypeFromName("byte")));
+    }
+}
