8a14b355162810bdd7e974def8c5b0b794b54c40
BradleyWood <bradley.wood@uoit.net>
2018-07-16 11:57:53 -0400

    test field, variable, function resolution

--- src/test/java/org/bw/tl/compiler/resolve/SymbolResolutionTest.java
+++ src/test/java/org/bw/tl/compiler/resolve/SymbolResolutionTest.java
@@ -1,28 +1,53 @@
 package org.bw.tl.compiler.resolve;
 
+import lombok.Data;
+import org.bw.tl.antlr.ast.Module;
+import org.bw.tl.compiler.Compiler;
+import org.bw.tl.primer.ModifierPrimer;
 import org.junit.Assert;
 import org.junit.Test;
-import org.objectweb.asm.Type;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.bw.tl.TestUtilities.getResolver;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Map;
 
-public class SymbolResolutionTest {
+import static org.bw.tl.TestUtilities.getModuleFromFile;
+
+@RunWith(Parameterized.class)
+public @Data class SymbolResolutionTest {
+
+    private final String filePath;
 
     @Test
     public void testResolveFunction() {
-        final SymbolResolver resolver = getResolver("package testModule; fun testFunction(): int {}" +
-                "fun anotherTest() {} \n" +
-                "fun funWithParams(boolean a, int b) {}\n");
+        final Module module = getModuleFromFile(filePath);
 
-        final Type modType = Type.getType("LtestModule;");
+        Assert.assertNotNull(module);
 
-        Assert.assertEquals(Type.INT_TYPE, resolver.resolveFunction(modType, "testFunction").getTypeDescriptor().getReturnType());
-        Assert.assertEquals(Type.VOID_TYPE, resolver.resolveFunction(modType, "anotherTest").getTypeDescriptor().getReturnType());
+        final ModifierPrimer modifierPrimer = new ModifierPrimer();
+        modifierPrimer.prime(module);
 
-        Assert.assertEquals(Type.VOID_TYPE, resolver.resolveFunction(modType, "funWithParams", Type.BOOLEAN_TYPE,
-                Type.INT_TYPE).getTypeDescriptor().getReturnType());
+        final Compiler compiler = new Compiler(module);
 
-        Assert.assertNull(resolver.resolveFunction(modType, "funWithParams", Type.BOOLEAN_TYPE,
-                Type.LONG_TYPE));
+        final Map<String, byte[]> o = compiler.compile();
+
+        Assert.assertEquals(0, compiler.getErrors().size());
+        Assert.assertNotNull(o);
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> parameters() throws IOException {
+        final LinkedList<Object[]> parameters = new LinkedList<>();
+
+        Files.walk(Paths.get("testData/resolution/")).filter(p -> p.toString().endsWith(".tl"))
+                .map(Path::toString).forEach(p -> parameters.add(new Object[] { p }));
+
+        return parameters;
     }
 }
