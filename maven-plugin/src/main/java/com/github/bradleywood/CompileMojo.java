578c82338a2ccd94343369545549c96f0085de52
BradleyWood <bradley.wood@uoit.net>
2018-07-19 20:30:39 -0400

    implement compiler mojo

--- maven-plugin/src/main/java/com/github/bradleywood/CompileMojo.java
+++ maven-plugin/src/main/java/com/github/bradleywood/CompileMojo.java
@@ -5,8 +5,13 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.bw.tl.util.CompileUtilities;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Mojo(name = "compile", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyResolution = ResolutionScope.COMPILE)
 public class CompileMojo extends AbstractMojo {
@@ -22,6 +27,25 @@ public class CompileMojo extends AbstractMojo {
 
     @Override
     public void execute() throws CompilationFailureException {
+        try {
+            final Map<String, byte[]> classMap = CompileUtilities.compile(sourceDirectory, classpath);
 
+            if (classMap == null)
+                throw new CompilationFailureException();
+
+            for (final Map.Entry<String, byte[]> entry : classMap.entrySet()) {
+                final File file = new File(outputDirectory, entry.getKey().replace(".", "/") + ".class");
+                final File parent = file.getParentFile();
+
+                if (!parent.exists())
+                    parent.mkdirs();
+
+                final FileOutputStream fos = new FileOutputStream(file);
+                fos.write(entry.getValue());
+                fos.close();
+            }
+        } catch (IOException e) {
+            throw new CompilationFailureException();
+        }
     }
 }
