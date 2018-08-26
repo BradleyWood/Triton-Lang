ef446528ca24660766df040d0c0d84e9f4c6cd60
BradleyWood <bradley.wood@uoit.net>
2018-08-25 21:00:07 -0400

    test the values returned from the Interpreter's eval function

---
+++ script-engine/src/test/java/org/triton/interpreter/InterpreterTest.java
@@ -0,0 +1,25 @@
+package org.triton.interpreter;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.triton.iterpreter.TritonInterpreter;
+
+import javax.script.ScriptException;
+
+public class InterpreterTest {
+
+    private final TritonInterpreter interpreter = new TritonInterpreter();
+
+    @Test
+    public void testEvalReturnValue() throws ScriptException {
+        Assert.assertEquals(10, interpreter.eval("5 + 5"));
+
+        Assert.assertEquals("Hello, World", interpreter.eval("\"Hello, World\""));
+
+        Assert.assertEquals(1000, interpreter.eval("var a = 1000; a"));
+
+        Assert.assertEquals(11, interpreter.eval("5 + 5; 11"));
+
+        Assert.assertNull(interpreter.eval(""));
+    }
+}
