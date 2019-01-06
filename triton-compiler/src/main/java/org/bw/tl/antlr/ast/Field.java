926f95553795eec0072b1d75e49bc2c67ba729fa
BradleyWood <bradley.wood@uoit.net>
2019-01-06 05:16:52 -0500

    allow modification to field type

--- triton-compiler/src/main/java/org/bw/tl/antlr/ast/Field.java
+++ triton-compiler/src/main/java/org/bw/tl/antlr/ast/Field.java
@@ -6,11 +6,17 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public @Data class Field extends ModifiableStatement {
 
+    private TypeName type;
     private final String name;
-    private final TypeName type;
     private final Expression initialValue;
 
     private boolean isConstant = false;
+
+    public Field(final String name, final TypeName type, final Expression initialValue) {
+        this.name = name;
+        this.type = type;
+        this.initialValue = initialValue;
+    }
 
     @Override
     public void accept(final ASTVisitor visitor) {
