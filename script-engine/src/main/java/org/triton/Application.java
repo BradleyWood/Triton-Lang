c130bd0fac4f55678181bf6ff618d0fe3f1c1395
BradleyWood <bradley.wood@uoit.net>
2019-01-06 05:58:12 -0500

    add repl (read-eval-print-loop)

--- script-engine/src/main/java/org/triton/Application.java
+++ script-engine/src/main/java/org/triton/Application.java
@@ -4,15 +4,28 @@ import org.triton.iterpreter.TritonInterpreter;
 
 import javax.script.ScriptException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
+        final TritonInterpreter interpreter = new TritonInterpreter();
+
         if (args.length <= 0) {
-            System.err.println("Missing path argument");
+            final Scanner sc = new Scanner(System.in);
+
+            while (true) {
+                final String line = sc.nextLine();
+
+                try {
+                    interpreter.exec(line);
+                } catch (ScriptException e) {
+                    e.printStackTrace();
+                }
+            }
+
         } else {
             try {
-                final TritonInterpreter interpreter = new TritonInterpreter();
                 interpreter.execFile(args[0]);
             } catch (IOException e) {
                 System.err.println(String.format("Cannot open file: \"%s\" for reading", args[0]));
